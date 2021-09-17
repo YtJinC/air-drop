8afc7344182f003daf50c1430093a49d1014886a
Guardiany <1204493146@qq.com>
2021-09-17 11:16:45 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Dear users:
+        Since 4EVER-HOSTING went live on 16th August it has shown great popularity in a short period of time, with many applications already deployed by developers in 4EVERLAND Web Hosting Services.<br>
+        Today, we are pleased to announce the launch of the 4EVERLAND First Landing Event with 50 million 4EVER tokens and limited NFT rewards up for grabs.<br>
+        4EVERLAND's First Landing aims to reach outstanding developers and help them efficiently develop Web3.0 applications, encouraging the deployment of applications in 4EVERLAND.<br>
+        Developers from all project stages are welcome to join this global event and all you have to do is deploy your website in 4EVERLAND to get a share of the massive reward pool.<br>
+     亲爱的用户:自从8月16日4EVER-HOSTING上线以来，它在短时间内就显示出了巨大的人气，许多应用程序已经被开发者部署在4EVERLAND Web Hosting <br>Services中。今天，我们很高兴地宣布，4EVERLAND首航活动正式启动，提供5000万枚4EVER代币和有限的非金融奖励。4EVERLAND的First <br>Landing旨在接触优秀的开发者，帮助他们高效开发Web3.0应用，鼓励应用在4EVERLAND的部署。我们欢迎来自各个项目阶段的开发商加入这一全球活动，您所要做的就是在4EVERLAND部署您的网站，以获得巨大的回报。<br>
+     参与方式：https://baijiahao.baidu.com/s?id=1710949686255173233&wfr=spider&for=pc <br>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '如何获取空投',
+  props: {
+    msg: String
+  }
+}
+</script>
